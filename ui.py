4f88b703d6a8637bbcbd3ec73834906229897707
agorq <alperengurses@outlook.com.tr>
2018-10-19 00:58:26 +0300

    Finished PyQt User Interface seperated matplotlib section

--- ui.py
+++ ui.py
@@ -1,10 +1,9 @@
 import sys
  
-from PyQt5.QtWidgets import QApplication, QMainWindow, QMenu, QVBoxLayout, QSizePolicy, QAction, QWidget, QInputDialog, QLineEdit, QFileDialog, QFrame
-
-from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.figure import Figure
-import matplotlib.pyplot as plt
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
+from histogram import Histogram
  
 import random
  
@@ -15,23 +14,56 @@ class App(QMainWindow):
         self.left = 0
         self.top = 0
         self.title = 'Histogram Equalization'
-        self.width = 1280
-        self.height = 720
+        self.width = 1920
+        self.height = 1080
+        self.inputImagePath = ""
+        self.targetImagePath = ""
+        self.inputLabel = ""
+        self.targetLabel = ""
+        self.resultLabel = ""
         self.initUI()
  
     def initUI(self):
+        self.inputLabel = QLabel()
+        self.inputLabel.setAlignment(Qt.AlignCenter)
+        self.targetLabel = QLabel()
+        self.targetLabel.setAlignment(Qt.AlignCenter)
+        self.resultLabel = QLabel()
+        self.resultLabel.setAlignment(Qt.AlignCenter)
+
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
 
         self.initOptionBar()
  
-        m = PlotCanvas(self, width=5, height=4)
+        central = QWidget()
+        hbox = QHBoxLayout(central)
 
-        widget = QWidget()
-        vbox = QVBoxLayout(widget)
-        vbox.addWidget(m)
+        left = QGroupBox("Input")
+        left.width = 640
+        center = QGroupBox("Target")
+        center.width = 640
+        right = QGroupBox("Result")
+        right.width = 640
+        
+        m = Histogram(left, width=5, height=4)
+        lvbox = QVBoxLayout(left)
+        lvbox.addWidget(self.inputLabel)
+        lvbox.addWidget(m)
 
-        self.setCentralWidget(widget)
+        cvbox = QVBoxLayout(center)
+        cvbox.addWidget(self.targetLabel)
+        #cvbox.addWidget(m)
+
+        rvbox = QVBoxLayout(right)
+        #rvbox.addWidget(self.inputLabel)
+        #rvbox.addWidget(m)
+
+        hbox.addWidget(left)
+        hbox.addWidget(center)
+        hbox.addWidget(right)
+
+        self.setCentralWidget(central)
 
         self.show()
 
@@ -40,45 +72,35 @@ class App(QMainWindow):
         OptionBar = self.menuBar()
 
         FileTab = OptionBar.addMenu('File')
+        Equalize = OptionBar.addAction('Equalize Histogram')
 
         InputAction = QAction('Open Input', self)
-        InputAction.triggered.connect(self.openImage)
+        InputAction.triggered.connect(self.openInputImage)
         TargetAction = QAction('Open Target', self)
-        TargetAction.triggered.connect(self.openImage)
+        TargetAction.triggered.connect(self.openTargetImage)
 
         FileTab.addAction(InputAction)
         FileTab.addAction(TargetAction)
 
-    def openImage(self):
+
+    def openInputImage(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "", "JPG files (*.jpg)", options=options)
+        fileName, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "", "PNG files (*.png)", options=options)
         if fileName:
-            print(fileName)
+            inputPixmap = QPixmap(fileName)
+            self.inputLabel.setPixmap(inputPixmap)
+
+    def openTargetImage(self):
+        options = QFileDialog.Options()
+        options |= QFileDialog.DontUseNativeDialog
+        fileName, _ = QFileDialog.getOpenFileName(self, "QFileDialog.getOpenFileName()", "", "PNG files (*.png)", options=options)
+        if fileName:
+            targetPixmap = QPixmap(fileName)
+            self.targetLabel.setPixmap(targetPixmap)
  
  
-class PlotCanvas(FigureCanvas):
- 
-    def __init__(self, parent=None, width=5, height=4, dpi=100):
-        fig = Figure(figsize=(width, height), dpi=dpi)
-        self.axes = fig.add_subplot(111)
- 
-        FigureCanvas.__init__(self, fig)
-        self.setParent(parent)
- 
-        FigureCanvas.setSizePolicy(self,
-                QSizePolicy.Expanding,
-                QSizePolicy.Expanding)
-        FigureCanvas.updateGeometry(self)
-        self.plot()
- 
- 
-    def plot(self):
-        data = [random.random() for i in range(25)]
-        ax = self.figure.add_subplot(111)
-        ax.plot(data, 'r-')
-        ax.set_title('PyQt Matplotlib Example')
-        self.draw()
+
  
 if __name__ == '__main__':
     app = QApplication(sys.argv)
